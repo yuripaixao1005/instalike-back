5e89ce17a6a577ef077236e290c5d45b904e3d80
yuripaixao1005 <yuripaixao1005@gmail.com>
2024-11-18 21:44:20 -0300

    Primeira versão do projeto

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app =  express();
+app.listen(3000, () => {
+  console.log("servidor escutando");
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).send("Boas vindas à imersão");
+});
